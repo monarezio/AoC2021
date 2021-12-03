f692d1331dee27c9a1533c041c3403572dcb6697
samuel.kodytek@guided.cz <MartinSmrdi987>
2021-12-03 20:25:31 +0100

    Day 2 👍

--- day2.R
+++ day2.R
@@ -10,3 +10,16 @@ depth <- aggregated_input[aggregated_input[, 1] == 'down',][, 2] -
 horizontal * depth
 
 # Part 2
+input <- read.csv("day2_sample.txt", header = FALSE, sep = " ")
+input$V3 <- 0
+input[input$V1 == 'down', ]$V3 <- input[input$V1 == 'down', ]$V2
+input[input$V1 == 'up', ]$V3 <- -input[input$V1 == 'up', ]$V2
+
+input$Aim <- cumsum(input$V3)
+
+head(input)
+
+horizontal <- sum(input[input$V1 == 'forward',]$V2)
+depth <- sum(input[input$V1 == 'forward',]$V2 * input[input$V1 == 'forward',]$Aim)
+
+horizontal * depth
